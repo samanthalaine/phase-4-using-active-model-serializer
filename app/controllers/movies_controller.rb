11ea63964f64923eb5117a1015d793c78fb20922
Samantha Laine <samanthalaine8@gmail.com>
2021-09-23 18:42:34 -0400

    completed assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,11 +6,24 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
-  def show
-    movie = Movie.find(params[:id])
-    render json: movie
-  end
 
+def show
+  movie = Movie.find(params[:id])
+  render json: movie
+end
+
+
+def summary
+  movie = Movie.find(params[:id])
+  render json: movie, serializer: MovieSummarySerializer
+end
+
+def summaries
+  movies = Movie.all
+  render json: movies, each_serializer: MovieSummarySerializer
+end
+
+  
   private
 
   def render_not_found_response
